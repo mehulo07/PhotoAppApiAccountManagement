8db72f3c5392565f22ed131bf6e74d659c3934a2
mehulo07 <mehulmakwana339@gmail.com>
2020-11-19 14:46:22 +0530

    Initial commit

---
+++ PhotoAppApiAccountManagement/src/main/java/com/mehulsblog/PhotoApp/api/account/ui/controllers/AccountController.java
@@ -0,0 +1,16 @@
+package com.mehulsblog.PhotoApp.api.account.ui.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+	@GetMapping("/status/check")
+	public String status() {
+		return "Account Service is Working..";
+	}
+
+}
